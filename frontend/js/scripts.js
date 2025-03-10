0a3d7ed073a0a51409eb61407d1531ebc618670c
Andrew Morse <amorse@dal.ca>
2025-03-10 00:26:25 -0300

    test api

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -14,7 +14,7 @@ async function getRecipe() {
         let response;
 
         if(checked.length == 0) {
-            response = await fetch(`api/random`);
+            response = await fetch(`netlify/functions/api/random`);
         } else {
             response = await fetch(`api/random?param=${checked.join(',')}`);
         }
